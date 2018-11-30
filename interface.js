222c1402e4bd7c09fd0bd88dc7f70c4c6ec9e3e7
kiedunne <kie.dunne@gmail.com>
2018-11-30 10:40:51 +0000

    Sorted interface.js issue

--- interface.js
+++ interface.js
@@ -1,18 +1,50 @@
-// $(document).ready(function() {
-//   var thermostat = new Thermostat();
-//   updateTemperature();
-//
-//   $('#temp-up').click(function() {
-//     thermostat.turn_up();
-//     updateTemperature();
-//   });
-//
-//   $('#temp-down').click(function() {
-//     thermostat.turn_down();
-//     updateTemperature();
-//   });
-//
-//   function updateTemperature() {
-//     $('#temperature').text(thermostat.temp);
-//   };
-// });
+
+  $(document).ready(function() {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+    })
+    var thermostat = new Thermostat();
+     updateTemperature();
+
+  $('#temperature-up').on('click', function() {
+    thermostat.turn_up();
+     updateTemperature();
+    });
+
+  $('#temperature-down').click(function() {
+    thermostat.turn_down();
+     updateTemperature();
+    });
+
+   $('#reset').click(function() {
+     thermostat.reset();
+      updateTemperature();
+   });
+
+   $('#ps_on').click(function() {
+     thermostat.ps_on();
+     $('#power-saving-status').text('powersaver on')
+      updateTemperature();
+   });
+
+   $('#ps_off').click(function() {
+     thermostat.ps_off();
+     $('#power-saving-status').text('powersaver off')
+      updateTemperature();
+   });
+
+   function updateTemperature() {
+     $('#temperature').text(thermostat.temp);
+     thermostat.check_usage();
+     $('#temperature').attr('class', thermostat.usage);
+     $('#temperature').text(thermostat.temp);
+  };
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+    });
+  });
+});
